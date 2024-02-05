c5c57d134fdab1b46a3ca9c0031d55a04d66ffa5
Tushar SoftCoded <tusharsoftcoded@gmail.com>
2024-02-05 09:55:14 +0530

    Values and Variables

---
+++ index05Feb.js
@@ -0,0 +1,10 @@
+// LECTURE: Values and Variables
+
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions).
+const country = 'India';
+const continent = 'Asia';
+let population = 1428.62
+
+// 2. Log their values to the console
+console.log(country, continent, population)
