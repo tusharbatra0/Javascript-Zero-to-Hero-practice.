d90b7e4f08824962d6b337e67a5226b48723257a
Tushar SoftCoded <tusharsoftcoded@gmail.com>
2024-02-10 17:00:28 +0530

    Equality Operators Questions.

---
+++ EqualityOperators.js
@@ -0,0 +1,43 @@
+// LECTURE: Equality Operators: == vs. ===
+const country = 'India';
+const continent = 'Asia';
+let population = 1428.62;
+const isIsland = false;
+const language = 'Hindi'
+
+// 1. Declare a variable 'numNeighbours' based on a prompt input like this:
+// prompt('How many neighbour countries does your country
+// have?');
+let numNeighbours = "0";
+
+// 2. If there is only 1 neighbour, log to the console 'Only 1 border!' (use loose equality
+// == for now)
+
+// 3. Use an else-if block to log 'More than 1 border' in case 'numNeighbours'
+// is greater than 1
+
+// 4. Use an else block to log 'No borders' (this block will be executed when
+// 'numNeighbours' is 0 or any other value)
+
+// 5. Test the code with different values of 'numNeighbours', including 1 and 0.
+
+// 6. Change == to ===, and test the code again, with the same values of
+// 'numNeighbours'. Notice what happens when there is exactly 1 border! Why
+// is this happening?
+
+// 7. Finally, convert 'numNeighbours' to a number, and watch what happens now
+// when you input 1
+
+numNeighbours = parseInt(numNeighbours)
+
+if (numNeighbours === 1) {
+    console.log('Only 1 border!')
+}
+else if (numNeighbours > 1) {
+    console.log(`There is more than 1 border`)
+}
+else if (numNeighbours === 0) {
+    console.log(`There is no border`)
+}
+// 8. Reflect on why we should use the === operator and type conversion in this
+// situation
