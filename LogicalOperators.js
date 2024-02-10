db0c312bb3bb88b59413623ff2f5580dc9baab28
Tushar SoftCoded <tusharsoftcoded@gmail.com>
2024-02-10 17:11:54 +0530

    Understanding Logical Operators.

---
+++ LogicalOperators.js
@@ -0,0 +1,24 @@
+// LECTURE: Logical Operators
+
+const country = 'India';
+const continent = 'Asia';
+let population = 1428.62;
+const isIsland = false;
+const language = 'Hindi'
+
+// 1. Let's say Sarah is looking for a new country to live in. She wants to live in a
+// country that speaks english, has less than 50 million people and is not an
+// island.
+// 2. Write an if statement to help Sarah figure out if your country is right for her.
+// You will need to write a condition that accounts for all of Sarah's criteria. Take
+// your time with this, and check part of the solution if necessary.
+// 3. If yours is the right country, log a string like this: 'You should live in Portugal :)'. If
+// not, log 'Portugal does not meet your criteria :('
+if(language === "English" && population<50 && !isIsland){
+    console.log(`You should live in ${country}`)
+} else{
+    console.log(`${country} does not meet your criteria`)
+}
+// 4. Probably your country does not meet all the criteria. So go back and temporarily
+// change some variables in order to make the condition true (unless you live in
+// Canada :D)
