b85e17994499f23945253c265eb988fa25603dd7
Tushar Batra <tusharbatra08@gmail.com>
2024-02-23 21:00:41 +0530

    Basic Array Operations.

---
+++ BasicArrayOperations.js
@@ -0,0 +1,30 @@
+// LECTURE: Basic Array Operations (Methods)
+// 1. Create an array containing all the neighbouring countries of a country of your
+// choice. Choose a country which has at least 2 or 3 neighbours. Store the array
+// into a variable called 'neighbours'
+const neighbours = ['China', 'Pakistan', 'Sri Lanka']
+// 2. At some point, a new country called 'Utopia' is created in the neighbourhood of
+// your selected country. So add it to the end of the 'neighbours' array
+const newNeighbour = 'Utopia';
+neighbours.push(newNeighbour)
+
+
+// 3. Unfortunately, after some time, the new country is dissolved. So remove it from
+// the end of the array
+neighbours.pop()
+console.log(neighbours)
+// 4. If the 'neighbours' array does not include the country ‘Germany’, log to the
+// console: 'Probably not a central European country :D'
+if (!neighbours.includes('Germany')) {
+    console.log('Probably not a central European country :D')
+}
+// 5. Change the name of one of your neighbouring countries. To do that, find the
+// index of the country in the 'neighbours' array, and then use that index to
+// change the array at that index position. For example, you can search for
+// 'Sweden' in the array, and then replace it with 'Republic of Sweden'.
+
+const indexofCountry = neighbours.indexOf('Pakistan');
+
+const newValue = 'Afghanistan';
+neighbours[indexofCountry] = newValue;
+console.log('New Value', neighbours);
